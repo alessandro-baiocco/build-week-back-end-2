7c4015e3c2a13ff6d4be9c8152ca77b660dc41fa
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-11-20 10:37:44 +0100

    first commit

---
+++ src/main/java/application/U5D16/U5D16Application.java
@@ -0,0 +1,13 @@
+package application.U5D16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5D16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5D16Application.class, args);
+	}
+
+}
---
+++ src/test/java/application/U5D16/U5D16ApplicationTests.java
@@ -0,0 +1,13 @@
+package application.U5D16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5D16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
