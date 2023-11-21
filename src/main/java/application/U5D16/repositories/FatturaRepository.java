bc55e601f5db6a1be9360bba17c09895975da151
Godwin <godwinmail6@gmail.com>
2023-11-21 11:28:12 +0100

    Created fattura service class

--- src/main/java/application/U5D16/repositories/FatturaRepository.java
+++ src/main/java/application/U5D16/repositories/FatturaRepository.java
@@ -1,5 +1,6 @@
 package application.U5D16.repositories;
 
+import application.U5D16.entities.Client;
 import application.U5D16.entities.Fattura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,5 @@ import java.util.UUID;
 @Repository
 public interface FatturaRepository extends JpaRepository<Fattura, UUID> {
 
-    Optional<Fattura> findById (UUID id);
+    Optional<Fattura> findByClient (UUID client);
 }
